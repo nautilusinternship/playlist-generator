0c854a447b3b8f977eaea1e805ce09e303eb4803
Laura Fang <68083672+laurazf@users.noreply.github.com>
2020-08-11 14:48:54 -0500

    laura testing
    just changed three lines at top to account for every payload only having two values

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.post("/",(req,res)=>{
     console.log('body is', req.body);
     var text = req.body["text"];
     console.log('text is', text);
-    payload_type = text.split(':')[0];
+    //payload_type = text.split(':')[0];
     console.log(payload_type);
-    genre = text.split(':')[1];
+    // genre = text.split(':')[1];
+    genre = text.split(':')[0];
     console.log(genre);
-    roundNumber = text.split(':')[2];
+    // roundNumber = text.split(':')[2];
+    roundNumber = text.split(':')[1];
     console.log(roundNumber);
 
     // MANIPULATE DATA AND SEND BACK TO NEMO
