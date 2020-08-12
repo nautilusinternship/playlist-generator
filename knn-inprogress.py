ffd97ec4ec018164596fbdea0d2aa6e4a46aa77c
Carina Lewandowski <carinal@princeton.edu>
2020-08-12 13:31:43 -0400

    knn parses payload string, runs knn, removes duplicates, returns uri string

--- knn-inprogress.py
+++ knn-inprogress.py
@@ -3,14 +3,29 @@ import pandas as pd
 import numpy as np
 import math
 import operator
+import random
+import sys
 
 #### Start of STEP 1
 # Importing data
 data = pd.read_csv('audio_data_set.csv')
 #### End of STEP 1
+# print(data.head(5))
 
-print(data.head(5))
+''' PARSE COMMAND LINE ARGS '''
+# sys.argv[1] for testing: 1~6nipZlJiUvI0I7lCf1Z7Li~5BBBxfYZuMVGuyh9L0LcVWjzE~VECTOR~0,0,0.8,0,0.1,0,0,0,0,0.55,0.8,0,0.6
+args_array = sys.argv[1].split('~VECTOR~')
+front = args_array[0].split('~') # round & uris seen
+round_num = int(front[0]) # round number
+length = len(front)
+uris_seen = front[1:length] # list of uris already seen by user
+uri_pref = uris_seen[0] # uri of song pref'd by user in this round
+tasteVector = list(map(float, args_array[1].split(','))) # current taste vector, to be updated by uri_pref
 
+# print('round: ' + str(round_num))
+# print('uris seen: ' + str(uris_seen))
+# print('uri pref: ' + str(uri_pref))
+# print('current tv: ' + str(tasteVector))
 
 # Defining a function which calculates euclidean distance between two data points
 def euclideanDistance(data1, data2, length):
@@ -68,7 +83,7 @@ def knn(trainingSet, testInstance, k):
     return neighbors
     #### End of STEP 3.5
 
-    def find_index(input): 
+def find_index(input): 
     o = open('audio_data_set.csv', 'r') 
     myData = csv.reader(o) 
     index = 0 
@@ -77,24 +92,54 @@ def knn(trainingSet, testInstance, k):
       if row[0] == input: 
         return index 
       else : index+=1
+if (round_num == 0):
+    genre = 'pop'
+    num_rows = 10
+    # get relevants columns (uri + selected genre)
+    uri_gen_df = data[['uri', genre]]
+    # sort by genre values & select top 10 rows
+    uri_gen_df = uri_gen_df.sort_values(by = genre, ascending=False).head(10)
+    # get 1st random row 
+    sample = random.sample(range(0, num_rows), 2)
+    rand_index1 = sample[0]
+    rand_row1 = uri_gen_df.iloc[rand_index1]
+    # get 2nd random row
+    rand_index2 = sample[1]
+    rand_row2 = uri_gen_df.iloc[rand_index2]
+    uri_str = rand_row1['uri'] + '~' + rand_row2['uri']
+else:
+    ''' CARINA'S CODE '''
+    ''' --------------------------------------------------- '''
+    # INSERT CODE TO UPDATE TASTE VECTOR
+    tasteVector = [[0,0,0.8,0,0.1,0,0,0,0,0.55,0.8,0,0.6]]
+    # RUN KNN WITH SPECIFIED K
+    test_df = pd.DataFrame(tasteVector)
+    if (round_num < 5):
+        k = (int(round_num) + 1) * 2
+    else:
+        k = (int(round_num) + 1) * 2 + 3
+    neighbors = knn(data.iloc[:,1:14], test_df, k)
+    uri_list = []
+    for n in neighbors:
+        uri_list.append(data.iloc[n]['uri'])
+    # REMOVE DUPLICATES
+    i = 0
+    while i < len(uri_list):
+        current_uri = uri_list[i]
+        if current_uri in uris_seen:
+            uri_list.remove(current_uri)
+            i -=1 # dec index b/c of removal
+        else:
+            i +=1
+    # RETURN STRING OF URIS (PICK TOP 2 OF REMAINING)
+    if (round_num < 5):
+        payload = '~'.join(uri_list[:2])
+    else:
+        payload = '~'.join(uri_list[:5])
+    print(payload)
 
-if (sys.argv[1] == 0)
-{
-    # pick out of top 10 songs
-    k = 10
-    genreColumn = data.columns.get_loc(sys.argv[0])
-    print(genreColumn)
-    testSet = [0] * 13
-    testSet[genreColumn] = 1
-    print(testSet)
-    test = pd.DataFrame(testSet)
-    # run knn to find 10 closest entries
-    neigh = knn(data, test, k) # neigh is a vector of row numbers
-    # put results in dataframe and grab two random rows
-    '''INSERT CODE HERE'''
-}
-else {
-    for x in range(len(sys.argv)):
+    ''' --------------------------------------------------- '''
+    """ for x in range(len(sys.argv)):
         # get row number using uri and add all values in the row's other columns to taste vector
         tasteVector = [0] * 13
         songs
@@ -115,8 +160,7 @@ else {
         '''INSERT CODE HERE'''    
     # print two random songs from the dataframe of new songs
     ''' INSERT CODE HERE'''
-}
-
+ """
 
 
 
