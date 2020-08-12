ef5dbbd169e08d449fcb6c1bd216cce68a82ca7e
Laura Fang <68083672+laurazf@users.noreply.github.com>
2020-08-12 00:08:57 -0500

    testing testing...

--- script2.py
+++ script2.py
@@ -2,5 +2,4 @@ import sys
 #print('Testing! \n')
 #print('First param: '+sys.argv[1]+'\n')
 #print('Second param: '+sys.argv[2])
-
-print(sys.argv[1]+'~'+sys.argv[2])
+print(sys.argv[1]+'~'+sys.argv[2]+'~third arg'+'~fourth arg' +'~fifth arg')
