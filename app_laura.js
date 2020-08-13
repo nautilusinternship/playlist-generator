7230248eb3c20204603255c181a3edb74818412d
Laura Fang <68083672+laurazf@users.noreply.github.com>
2020-08-13 16:33:11 -0500

    Rough draft of final code (needs to be tested)

--- app_laura.js
+++ app_laura.js
@@ -6,29 +6,14 @@ app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-var songURI = "Null";
-var roundNumber = 0;
-var genre = "Null";
-
 app.post("/",(req,res)=>{
 
     // PARSE USER INPUT FROM NEMO
-    //console.log("post block entered.");
-    //console.log("receiving data...");
-    //console.log('body is', req.body);
     var text = req.body["text"];
-    //console.log('text is', text);
-    // payload_type = text.split(':')[0];
-    // console.log(payload_type);
-    genreOrLink = text.split(':')[0];
-    //console.log(genre);
-    roundNumber = text.split(':')[1];
-    //console.log(roundNumber);
-
     // MANIPULATE DATA AND SEND BACK TO NEMO
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('python', ['script2.py', genreOrLink, roundNumber]); 
+    const python = spawn('python', ['knn-inprogress.py', text]); 
     // collect data from script. Takes whatever is printed from python script knn.py and post on localhost:3000
     python.stdout.on('data', function (data) {
         console.log("Pipe data from python script...");
@@ -48,7 +33,7 @@ app.get('/', (req, res) => {
     console.log("get block entered.");
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('python', ['script2.py', genreOrLink, roundNumber]); 
+    const python = spawn('python', ['knn-inprogress.py', text]); 
     // collect data from script. Takes whatever is printed from python script knn.py and post on localhost:3000
     python.stdout.on('data', function (data) {
         console.log("Pipe data from python script...");
