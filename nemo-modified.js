02bb654f3971a924261e5772174f2490c408483f
Carina Lewandowski <carinal@princeton.edu>
2020-08-13 19:46:48 -0400

    small typo fix

--- nemo-modified.js
+++ nemo-modified.js
@@ -7,8 +7,8 @@ let rounds = 0;
 const start = (say, sendButton) => {
     say("In this game, you will choose your preference from provided songs so\
 	we can help recommend other music you might like :)").then(() => {
-        sendButton('Are you ready?', [{ title: 'Yes', payload: 'Ready' }, 'No']);
-    });
+            sendButton('Are you ready?', [{ title: 'Yes', payload: 'Ready' }, 'No']);
+        });
 };
 
 
@@ -28,7 +28,7 @@ const state = (payload, say, sendButton) => {
         // post genre and round number to app.js
         axios({
             method: 'post',
-            baseURL: 'https://playlist-generatr.herokuapp.com', 
+            baseURL: 'https://playlist-generatr.herokuapp.com',
             url: '/',
             'Content-Type': 'application/json',
             data: {
@@ -39,18 +39,18 @@ const state = (payload, say, sendButton) => {
             // get the returned data here, a new pair of song URLs
             var link1 = resp.data.split('~')[0];
             var link2 = resp.data.split('~')[1];
-        
-        // display song previews; user selects preference
-        say("Please select one of the following options.").then(() => {
-            say('https://open.spotify.com/embed/track/'+link1).then(() => {
-                say('https://open.spotify.com/embed/track/'+link2).then(() => {                            
-                    // rounds += 1;
+
+            // display song previews; user selects preference
+            say("Please select one of the following options.").then(() => {
+                say('https://open.spotify.com/embed/track/' + link1).then(() => {
+                    say('https://open.spotify.com/embed/track/' + link2).then(() => {
+                        // rounds += 1;
                         round++;
                         var str = "Round " + round + ": I like..."
                         sendButton(str, [{
                             title: 'Track 1 better',
-                        payload: round+'~'+link1+'~'+link2+'~VECTOR~'
-                        }, { title: 'Track 2 better', payload: round+'~'+link2+'~'+link1+'~VECTOR~'}]);
+                            payload: round + '~' + link1 + '~' + link2 + '~VECTOR~'
+                        }, { title: 'Track 2 better', payload: round + '~' + link2 + '~' + link1 + '~VECTOR~' }]);
                     });
                 });
             });
@@ -58,10 +58,10 @@ const state = (payload, say, sendButton) => {
     }
 
     // if not first round (selecting links)
-    if (payload_type == 'https' && round < 5){
+    if (round > 0 && round < 5) {
         axios({
             method: 'post',
-            baseURL: 'https://playlist-generatr.herokuapp.com', 
+            baseURL: 'https://playlist-generatr.herokuapp.com',
             url: '/',
             'Content-Type': 'application/json',
             data: {
@@ -73,20 +73,20 @@ const state = (payload, say, sendButton) => {
             var link1 = resp.data.split('~')[0];
             var link2 = resp.data.split('~')[1];
             var data = resp.data.split('~');
-            var oldData = data.splice(0,2);
+            var oldData = data.splice(0, 2);
             var dataToSend = oldData.join('~');
-        
-        // display song previews; user selects preference
-        say("Please select one of the following options.").then(() => {
-            say('https://open.spotify.com/embed/track/'+link1).then(() => {
-                say('https://open.spotify.com/embed/track/'+link2).then(() => {                            
-                    // rounds += 1;
+
+            // display song previews; user selects preference
+            say("Please select one of the following options.").then(() => {
+                say('https://open.spotify.com/embed/track/' + link1).then(() => {
+                    say('https://open.spotify.com/embed/track/' + link2).then(() => {
+                        // rounds += 1;
                         round++;
                         var str = "Round " + round + ": I like..."
                         sendButton(str, [{
                             title: 'Track 1 better',
-                            payload: round+'~'+link1+'~'+link2+'~'+dataToSend
-                        }, { title: 'Track 2 better', payload: round+'~'+link2+'~'+link1+'~'+dataToSend}]);
+                            payload: round + '~' + link1 + '~' + link2 + '~' + dataToSend
+                        }, { title: 'Track 2 better', payload: round + '~' + link2 + '~' + link1 + '~' + dataToSend }]);
                     });
                 });
             });
@@ -95,7 +95,7 @@ const state = (payload, say, sendButton) => {
 
     /* after picking preferences, 5 times, return results of kNN on user's 
        "taste vector" (5 recommended songs?) */
-       if (round > 4) {
+    if (round > 4) {
         // returns 5 song recs
         axios({
             method: 'post',
@@ -116,9 +116,10 @@ const state = (payload, say, sendButton) => {
             var rec5 = 'https://open.spotify.com/embed/track/' + resp.data.split('~')[4];
             say("Here is your recommended mini-playlist!").then(() => {
                 say(rec1 + "\n" + rec2 + "\n" + rec3 + "\n" + rec4 + "\n" + rec5).then(() => {
-                    sendButton("Play again?", [{ title: 'Yes', payload: 'Again' }, 'No']);                    })
+                    sendButton("Play again?", [{ title: 'Yes', payload: 'Again' }, 'No']);
                 })
             })
+        })
     }
 };
 
