caf9bebc6add528eee784283537f62af66518983
Carina Lewandowski <carinal@princeton.edu>
2020-08-14 00:23:38 -0400

    test file to trace through knn-inprogress

---
+++ test-payloads.py
@@ -0,0 +1,42 @@
+# ROUND 0 
+# --> user picks genre
+# --> payload = 0~[genre]~VECTOR~
+payload = '0~pop~VECTOR'
+
+# ROUND 1 
+# --> user picks b/w 2 links
+# --> payload = 1~[uri1]~[uri2]~VECTOR~
+uri1 = '6SpLc7EXZIPpy0sVko0aoU' # (pref)
+uri2 = '3kwgqoBqTwoAH4nT29TYrq'
+payload = '1~6SpLc7EXZIPpy0sVko0aoU~3kwgqoBqTwoAH4nT29TYrq~VECTOR~'
+
+# ROUND 2 
+# --> user picks b/w 2 links
+# --> payload = 2~[uri1]~[uri2]~[prev uris]~VECTOR~
+uri1 = '4bPQs0PHn4xbipzdPfn6du' 
+uri2 = '2H6txWVNy08LZYGmkHU511' # (pref)
+payload = '2~2H6txWVNy08LZYGmkHU511~4bPQs0PHn4xbipzdPfn6du~6SpLc7EXZIPpy0sVko0aoU~3kwgqoBqTwoAH4nT29TYrq~VECTOR~0.0,0.0,0.69,0.0,0.15,0.0,0.0,0.78,0.0,0.72,0.73,0.15,0.1'
+
+# ROUND 3 
+# --> user picks b/w 2 links
+# --> payload = 3~[uri1]~[uri2]~[prev uris]~VECTOR~
+uri1 = '3qRXuJ58B2wcNNMk5yyAMn' # (pref)
+uri2 = '4TIJ7zSBNejpoIPaWpWRKc' 
+payload = '3~3qRXuJ58B2wcNNMk5yyAMn~4TIJ7zSBNejpoIPaWpWRKc~2H6txWVNy08LZYGmkHU511~4bPQs0PHn4xbipzdPfn6du~6SpLc7EXZIPpy0sVko0aoU~3kwgqoBqTwoAH4nT29TYrq~VECTOR~0.0,0.0,0.845,0.0,0.075,0.0,0.0,0.56,0.0,0.69,0.8200000000000001,0.11,0.14500000000000002'
+
+# ROUND 4 
+# --> user picks b/w 2 links
+# --> payload = 4~[uri1]~[uri2]~[prev uris]~VECTOR~
+uri1 = '222MT5t80Pf0C1ItJ79sCF' 
+uri2 = '0a7BloCiNzLDD9qSQHh5m7' # (pref)
+payload = '4~0a7BloCiNzLDD9qSQHh5m7~222MT5t80Pf0C1ItJ79sCF~3qRXuJ58B2wcNNMk5yyAMn~4TIJ7zSBNejpoIPaWpWRKc~2H6txWVNy08LZYGmkHU511~4bPQs0PHn4xbipzdPfn6du~6SpLc7EXZIPpy0sVko0aoU~3kwgqoBqTwoAH4nT29TYrq~VECTOR~0.0,0.0,0.8416666666666667,0.0,0.024999999999999998,0.0,0.0,0.3666666666666667,0.0,0.5833333333333334,0.9199999999999999,0.2833333333333333,0.18833333333333332'
+
+# ROUND 5 
+# --> user picks b/w 2 links
+# --> payload = 4~[uri1]~[uri2]~[prev uris]~VECTOR~
+uri1 = '3fnqNxjN7o0tJe7zOQZV68' # (pref)
+uri2 = '14EXZEog1hW97d6yz8ejih' 
+payload= '5~3fnqNxjN7o0tJe7zOQZV68~14EXZEog1hW97d6yz8ejih~0a7BloCiNzLDD9qSQHh5m7~222MT5t80Pf0C1ItJ79sCF~3qRXuJ58B2wcNNMk5yyAMn~4TIJ7zSBNejpoIPaWpWRKc~2H6txWVNy08LZYGmkHU511~4bPQs0PHn4xbipzdPfn6du~6SpLc7EXZIPpy0sVko0aoU~3kwgqoBqTwoAH4nT29TYrq~VECTOR~0.0,0.0,0.9604166666666667,0.0,0.0062499999999999995,0.0,0.0,0.09166666666666667,0.0,0.7158333333333334,0.92,0.10833333333333334,0.11458333333333334'
+
+# MINI PLAYLIST
+uris = '3Ho0uOyCz245GWUqlWrkf5~1y8EEqXNbi7LtBpPLMPRui~08mG3Y1vljYA6bvDt4Wqkj~7lRlq939cDG4SzWOF4VAnd~2imAA5ofcn4dCRVNjKyEq3'
