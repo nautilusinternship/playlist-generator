606bb38bad2c8f3c9b9e2a317b3f1bc89b21a41a
Carina Lewandowski <carinal@princeton.edu>
2020-08-14 00:30:55 -0400

    fixing get block

--- app_laura.js
+++ app_laura.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-app.post("/",(req,res)=>{
+app.post("/", (req, res) => {
 
     // PARSE USER INPUT FROM NEMO
     var textdata = "Null";
@@ -15,7 +15,7 @@ app.post("/",(req,res)=>{
     // MANIPULATE DATA AND SEND BACK TO NEMO
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('python', ['knn-inprogress.py', textdata]); 
+    const python = spawn('python', ['knn-inprogress.py', textdata]);
     // collect data from script. Takes whatever is printed from python script knn.py and post on localhost:3000
     python.stdout.on('data', function (data) {
         console.log("Pipe data from python script...");
@@ -34,17 +34,27 @@ app.post("/",(req,res)=>{
 app.get('/', (req, res) => {
     console.log("get block entered.");
     var dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn('python', ['knn-inprogress.py', textdata]); 
-    // collect data from script. Takes whatever is printed from python script knn.py and post on localhost:3000
-    python.stdout.on('data', function (data) {
-        console.log("Pipe data from python script...");
-        dataToSend = data.toString();
-    });
-    // send data to browser
-    python.on('close', (code) => {
-        console.log('child process close all stdio with code ${code}');
-        res.send(dataToSend)
-    });
-}); 
+    try {
+        // spawn new child process to call the python script
+        const python = spawn('python', ['knn-inprogress.py', textdata]);
+        // collect data from script. Takes whatever is printed from python script knn.py and post on localhost:3000
+        python.stdout.on('data', function (data) {
+            console.log("Pipe data from python script...");
+            dataToSend = data.toString();
+        });
+        // send data to browser
+        python.on('close', (code) => {
+            console.log('child process close all stdio with code ${code}');
+            res.send(dataToSend)
+        });
+    }
+    catch {
+        // send data to browser
+        dataToSend = 'No data from nemobot at this time.'
+        python.on('close', (code) => {
+            console.log('child process close all stdio with code ${code}');
+            res.send(dataToSend)
+        });
+    }
+});
 app.listen(app.get('port'), () => console.log('example app listening on: port ' + app.get('port')))
