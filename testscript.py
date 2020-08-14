9eb7a6b78d90fec6e5249e818530c839c302c595
Carina Lewandowski <carinal@princeton.edu>
2020-08-14 02:10:49 -0400

    testingggg

--- testscript.py
+++ testscript.py
@@ -1,5 +1,14 @@
+# Importing libraries
+import pandas as pd
+import numpy as np
+import math
+import operator
+import random
 import sys
 
+#### Start of STEP 1
+# Importing data
+data = pd.read_csv('audio_data_set.csv')
 args_array = sys.argv[1].split('~VECTOR~')
 front = args_array[0].split('~') # round & uris seen
 round_num = int(front[0]) # round number
@@ -15,4 +24,17 @@ else:
     taste_vector = list(map(float, args_array[1].split(','))) # current taste vector, to be updated by uri_pref
 
 if (round_num == 0):
-    print('hello~hi')
+    num_rows = 10
+    # get relevants columns (uri + selected genre)
+    uri_gen_df = data[['uri', genre]]
+    # sort by genre values & select top 10 rows
+    uri_gen_df = uri_gen_df.sort_values(by = genre, ascending=False).head(10)
+    # get 1st random row 
+    sample = random.sample(range(0, num_rows), 2)
+    rand_index1 = sample[0]
+    rand_row1 = uri_gen_df.iloc[rand_index1]
+    # get 2nd random row
+    rand_index2 = sample[1]
+    rand_row2 = uri_gen_df.iloc[rand_index2]
+    uri_str = rand_row1['uri'] + '~' + rand_row2['uri']
+    print(uri_str)
